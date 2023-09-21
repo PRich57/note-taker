d3293b5688013a058e5105033eb7cabb356e30b3
PRich57 <pcrichards57@gmail.com>
2023-09-20 18:49:02 -0600

    Add post request

--- server.js
+++ server.js
@@ -17,7 +17,37 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
+// Create api route for /api/notes
+app.get('/api/notes', (req, res) => {
+  res.json(notesData);
+});
 
+app.post('/api/notes', (req, res) => {
+  // Let the user know that their POST request was received
+  console.info(`${req.method} request received`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object to be saved
+    const newNote = {
+      title,
+      text,
+    };
+    
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
